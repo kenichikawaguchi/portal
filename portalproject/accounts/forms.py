eeb17fa26ca496d174f12c462b219bb9bdee93d6
Kenichi Kawaguchi <sonatine@pontalk.com>
2024-10-29 07:12:12 +0900

    small bag fix

--- portalproject/accounts/forms.py
+++ portalproject/accounts/forms.py
@@ -13,3 +13,11 @@ class UsernameChangeForm(forms.ModelForm):
         for field in self.fields.values():
             field.widget.attrs['class'] = "form-control"
             field.widget.attrs['placeholder'] = "150 characters or fewer. Letters, digits and @/./+/-/_ only."
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        username_qs = CustomUser.objects.filter(username=username)
+        if username_qs.exists():
+            raise forms.ValidationError("Username already exists")
+        return username
+

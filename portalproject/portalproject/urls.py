0a68a0c34e2a825d96c44eb1f4cc026afffcce1e
Kenichi Kawaguchi <sonatine@pontalk.com>
2025-01-18 23:29:02 +0900

    Fix urls.py bug

--- portalproject/portalproject/urls.py
+++ portalproject/portalproject/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('maintenance-mode/', include('maintenance_mode.urls')),
-    path('', include('blog.urls')),
     path('accounts/profile/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
     path('tz_detect/', include('tz_detect.urls')),
     path('markdownx/', include('markdownx.urls')),
+    path('', include('blog.urls')),
 ]  +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
